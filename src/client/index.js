4b998b95786a33a5f22fb08c8a4fca8a256e166f
Naomi Hauret <naomi@troopers.email>
2017-10-12 15:57:06 +0200

    :sparkles: added default config for socket.io

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,11 @@
 import styles from "./styles/main.css"
+import io from "socket.io-client"
+import { SERVER_PORT } from "./../shared/config"
 
-console.log("hello world (from client")
+const socket = io.connect(`http://localhost:${SERVER_PORT}`);
+
+socket.on("news", (data) => {
+  console.log(data)
+  socket.emit("other event", {my: "data"})
+})
+console.log("hello world (from client)")
--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,18 @@
-console.log("hello world (from server")
+import { SERVER_PORT, CLIENT_PORT } from "./../shared/config"
+
+const express = require("express")
+const app = express()
+const server = require("http").Server(app)
+const io = require("socket.io")(server)
+
+app.use(express.static("dist"))
+
+server.listen(SERVER_PORT, () => {
+  console.log(`Server servin' from good ol' port ${SERVER_PORT}`)
+  console.log(`Client listenin' on port ${CLIENT_PORT}`)
+})
+
+io.on("connection", (socket) => {
+  socket.emit("news", {hello: "world"})
+  socket.on("other event", data => console.log)
+})
