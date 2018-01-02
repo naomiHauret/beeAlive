9e6027af9be74180084cc4e8085c799472fc83f6
Naomi Hauret <naomi@troopers.email>
2018-01-02 15:58:43 +0100

    rebase feature bee 3d model on master

--- src/client/js/Bee.js
+++ src/client/js/Bee.js
@@ -14,10 +14,9 @@ class Bee {
     this.mouse = new THREE.Vector2(0, 0)
     this.animate = this.animate.bind(this)
     this.deg = 0
-    this.x = 5
-    this.y = 5
-    this.z = 5
-    this.animationSpeed = Math.random() * (0.15 - 0.085) + 0.085
+    this.x = Math.random() * 8
+    this.y = Math.random() * 8
+    this.z = Math.random() * 8
     return this.initialize()
   }
 
@@ -37,33 +36,114 @@ class Bee {
     })
 
     this.materials.grey = new THREE.MeshBasicMaterial({
-      color: "#494846"
+      color: "#211f1e"
+    })
+
+    this.materials.brown = new THREE.MeshBasicMaterial({
+      color: "#2E2516"
     })
 
     // Groups
     this.bee = new THREE.Group()
     this.head = new THREE.Group()
     this.body = new THREE.Group()
+    this.abdomen = new THREE.Group()
     this.wings = new THREE.Group()
 
     // Geometries
-    this.headBaseGeometry = new THREE.DodecahedronGeometry(1, 0)
-    this.headMuzzleGeometry = new THREE.CylinderGeometry(0.5, 1, 2, 3)
+
+    // -- head
+    this.headBaseGeometry = new THREE.CylinderGeometry(0.4, 0.25, 0.25, 8)
+    this.headMuzzleGeometry = new THREE.TetrahedronGeometry(0.65, 1)
+
+    // -- body
+    // --- thorax
+    this.thoraxGeometry = new THREE.SphereGeometry(0.65, 8, 9, 1, 6.3, 3, 6.3)
+
+    // --- abdomen
+    this.abdomenSlice1Geometry = new THREE.CylinderGeometry(0.25, 0.6, 0.5, 8)
+    this.abdomenSlice2Geometry = new THREE.CylinderGeometry(0.6, 0.75, 0.45, 8)
+    this.abdomenSlice3Geometry = new THREE.CylinderGeometry(0.75, 0.75, 0.45, 8)
+    this.abdomenSlice4Geometry = new THREE.CylinderGeometry(0.75, 0.6, 0.45, 8)
+    this.abdomenSlice5Geometry = new THREE.CylinderGeometry(0.6, 0.25, 0.5, 8)
+
+    // --- sting
+    this.stingGeometry = new THREE.ConeGeometry(0.25, 0.35, 3)
+
+    // --- wings
+    this.wingLargeGeometry = new THREE.ConeGeometry(2, 0.25, 2, 0.25, 1, 1, 0.5)
 
     // Meshes
+    // -- head
     this.headBase = new THREE.Mesh(this.headBaseGeometry, this.materials.black)
-    this.headMuzzle = new THREE.Mesh(this.headMuzzleGeometry, this.materials.yellow)
+    this.headMuzzle = new THREE.Mesh(this.headMuzzleGeometry, this.materials.black)
+
+    // -- body
+    // --- thorax
+    this.thorax = new THREE.Mesh(this.thoraxGeometry, this.materials.brown)
+
+    // --- abdomen
+    this.abdomenSlice1 = new THREE.Mesh(this.abdomenSlice1Geometry, this.materials.black)
+    this.abdomenSlice2 = new THREE.Mesh(this.abdomenSlice2Geometry, this.materials.yellow)
+    this.abdomenSlice3 = new THREE.Mesh(this.abdomenSlice3Geometry, this.materials.black)
+    this.abdomenSlice4 = new THREE.Mesh(this.abdomenSlice4Geometry, this.materials.yellow)
+    this.abdomenSlice5 = new THREE.Mesh(this.abdomenSlice5Geometry, this.materials.black)
+
+    // --- sting
+    this.sting = new THREE.Mesh(this.stingGeometry, this.materials.black)
+
+    // --- wings
+    this.wingLeft = new THREE.Mesh(this.wingLargeGeometry, this.materials.white)
+    this.wingRight = new THREE.Mesh(this.wingLargeGeometry, this.materials.white)
 
     // Buildings groups
-
     this.head.add(this.headBase)
     this.head.add(this.headMuzzle)
-    this.headMuzzle.position.x = 1.5
-    this.headMuzzle.rotation.w = 90
 
+    this.abdomen.add(this.abdomenSlice1)
+    this.abdomen.add(this.abdomenSlice2)
+    this.abdomen.add(this.abdomenSlice3)
+    this.abdomen.add(this.abdomenSlice4)
+    this.abdomen.add(this.abdomenSlice5)
+    this.abdomen.add(this.sting)
+
+    this.wings.add(this.wingLeft)
+    this.wings.add(this.wingRight)
+
+    this.body.add(this.thorax)
+    this.body.add(this.abdomen)
+    this.body.add(this.wings)
     this.bee.add(this.head)
+    this.bee.add(this.body)
 
     // Positions
+    // -- head
+    this.head.position.y = 0.7
+    this.headMuzzle.position.y = 0.35
+
+    // -- body
+    // --- abdomen
+    this.abdomen.position.y = -0.5
+    this.abdomen.rotation.x = THREE.Math.degToRad(-25)
+
+    // ---wings
+    this.wings.rotation.x = THREE.Math.degToRad(-90)
+    this.wingLeft.position.x = 0.425
+    this.wingRight.position.x = -0.425
+    this.wingRight.position.z = -0.25
+    this.wingRight.rotation.y = THREE.Math.degToRad(180)
+
+    // --- sting
+    this.sting.position.y = -2.325
+    this.sting.rotation.x = THREE.Math.degToRad(180)
+
+    // ---- abdomen slices
+    this.abdomenSlice2.position.y = -0.475
+    this.abdomenSlice3.position.y = -0.95
+    this.abdomenSlice4.position.y = -1.425
+    this.abdomenSlice5.position.y = -1.9
+
+    // Bee
     this.bee.position.y = this.y
     this.bee.position.x = this.x
     this.bee.position.z = this.z
@@ -71,6 +151,7 @@ class Bee {
     // Shadows
     this.bee.castShadow = true
     this.bee.receiveShadow
+    this.bee.rotation.x = THREE.Math.degToRad(90)
 
     this.bee.traverse(node => {
       // list of meshes
@@ -83,23 +164,28 @@ class Bee {
     // this.animate()
     this.handlers()
 
-/*     TweenMax.to(
-      this.bee.position, this.animationSpeed, {
-        y: Math.random() * (1 - 0) + 0 > 0.5
-          ? `+=${Math.random() * 8}`
-          : `-=${Math.random() * 8}`,
-        x: Math.random() * (1 - 0) + 0 > 0.5
-          ? `+=${Math.random() * 8}`
-          : `-=${Math.random() * 8}`,
-        z: Math.random() * (1 - 0) + 0 > 0.5
-          ? `+=${Math.random() * 8}`
-          : `-=${Math.random() * 8}`,
-        ease: Power2.easeOut,
+    TweenMax.to(
+      this.bee.position, 1.25, {
+        y: Math.random() * 5,
         yoyo: true,
         repeat: -1
       }
-    ) */
+    )
 
+    TweenMax.to(
+      this.wingLeft.rotation, 0.035, {
+        z: 0.75,
+        yoyo: true,
+        repeat: -1
+      }
+    )
+    TweenMax.to(
+      this.wingRight.rotation, 0.035, {
+        z: 0.75,
+        yoyo: true,
+        repeat: -1
+      }
+    )
     return this.getModel()
   }
 
--- webpack.config.js
+++ webpack.config.js
@@ -30,12 +30,6 @@ module.exports = {
     rules: [
       // Javascript files
       {
-        enforce: "pre",
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader",
-      },
-      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader",
