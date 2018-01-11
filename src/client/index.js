86125fef2a8066a99a350cd90f380182fee82f2e
Trystan Eveno <trystan.eveno@lephare.com>
2018-01-11 01:08:20 +0100

    footer (bee number, 'send bee' button, popover help && many others things

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,26 @@
-import styles from "./styles/main.css"
-import io from "socket.io-client"
-import { SERVER_PORT } from "./../shared/config"
-import Scene from "./js/Scene"
+import styles from './styles/main.css';
+import domready from 'domready';
+import io from 'socket.io-client';
+import { SERVER_PORT } from './../shared/config';
+import Scene from './js/Scene';
+import 'whatwg-fetch';
 
-const socket = io.connect(`http://localhost:${SERVER_PORT}`)
+const socket = io.connect(`http://localhost:${SERVER_PORT}`);
+const mongo = 'http://localhost:3000/';
 
-socket.on("news", (data) => {
-  console.log(data)
-  socket.emit("other event", {my: "data"})
-})
+socket.on('news', (data) => {
+  console.log(data);
+  socket.emit('other event', { my: 'data' });
+});
 
-const ourScene = new Scene(false, false)
+domready(() => {
+  fetch(mongo +'findAll')
+    .then((response) => response.json())
+    .then((response) => {
+      console.log("WOW", response);
+    }
+  );
+  document.querySelector('#number').innerText = 15; // remplacer par variable qui init nombre d'abeilles
+});
+
+const ourScene = new Scene(false, false);
