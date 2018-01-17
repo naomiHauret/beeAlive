fbbcbf48d985ada7f0524f7fd982a34bb80c3b4c
Naomi Hauret <naomi@troopers.email>
2018-01-17 21:11:58 +0100

    :sparkles: added texts

--- src/client/index.js
+++ src/client/index.js
@@ -10,7 +10,7 @@ import Scene from "./js/Scene";
 import "whatwg-fetch";
 
 const loadingScreen = document.querySelector('[data-flag="loadingScreen"]');
-const ip = "192.168.0.18";
+const ip = "192.168.0.16";
 const url = `http://`+ ip +`:${SERVER_PORT}`
 const socket = io(url);
 const mongo = url +'/';
@@ -154,29 +154,31 @@ const getBees = (ourScene, compteur, add_bee, trigger) => {
 
           switch(ourScene.mode) {
             case "bad":
-              title = "💀 All life is gone";
-              text = "Is it Mars or Hell ? You don't really know. In fact, the only thing you're sure is that this place has nothing in common with the Earth you once knew. There's no life left here. With more than 99% of bees gone, over 90% of our food resources disappeared and things turned to something that reminds you of the Apocalypse. All life disappeared from Earth..."
+              title = "💀 Hallucination of a starving soul";
+              text = "Sometimes, when the hunger is too strong, I can see bees flying right in front of my eyes. It's stupid, since they're all either dead or flying through space with the richest ones. It's a feeling I both hate and love : buzzing all around my head, they cover this toxic wasteland I once called Earth. Their sound hide the screams of Men being devoured by others, for that's all there's left to eat: rocks, mud and the weakest of these wretched cannibalistic beings that once stand proudly as 'Humans'... They remind me of something we once were: a society in order."
             break;
 
             case "mediumBad":
-              title = "🔥 Apocalypse now";
-              text = "bleps"
+              title = "🔥 Hopes of a survivor";
+              text = "'Could things get worse ?' I wonder this everyday while looking for forgotten rations of food in the ruined houses and blown up buildings. That's when a bomb fell down from the skies to strike down what was still standing. We were at war. Everyone was. But it wasn't only a World Wide War: it's was also a war amoung people of the same country, cities, families. Hunger was striking us all, no matter how poor or rich we were. Humanity might take pride of being the most 'intelligent, cultivated species' of all, the top of food chain, if there wasn't anything to eat then there wasn't anything to be proud of. Especially when you're the one responsible for the disparition of all food source. If we were that superior, that intelligent, we could've prevent all this. But maybe it's not too late ? Maybe we can still save ourselves... I can see few bees, flying over what looks like a dandelion. What a sight to behold... They're so rare today, preciously captured (for what reason ? I'm not sure...). Our flying hope... Over 70% of what we ate was due to the work of bees : pollination. They fed us. They flew over Earth for more than 100,000 millions years, survived to a mass extinction, the same one that killed T-Rex. And yet, in 50 years, we nearly them all, causing our fall.."
             break;
 
             case "mediumGood":
-              title = "You're here ☟";
-               text= "blaps"
+              title = "☟ You're here";
+              text = "Almonds, apples, apricots, avocados, blueberries, cantaloupes, cashews, coffee, cranberries, cucumbers, eggplants, grapes, kiwis, mangoes, okra, peaches, pears, peppers, strawberries, tangerines, walnuts,  watermelons... I can't believe how many rare or expensive these things got lately. In the news, I read that more than 20% of domestic honeybees hives disappear each year and that without bees, we won't last more than 2 years. I wonder if it's true... yet hope I won't get to know the answer to that question one day."
+
+
             break;
 
             case "good":
               title = "🌻 What a wonderful world";
-              text = "feeling gooooood"
+              text = "It's so peaceful... I can understand why bees symbolize order, society and purity. When bees are well, Humanity is too. May things always be in that state."
             break;
           }
           swal({
             title,
             text,
-            button: "Back to the world"
+            button: "🔙 Back to the hive"
           });
           document.querySelector(".swal2-container").classList.add('no-background');
         });
